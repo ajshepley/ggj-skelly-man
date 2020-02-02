7e76db3c2263efbeb8426a6f6e52bfb6cc0651d0
Adam Shepley <ajshepley@gmail.com>
2020-02-02 01:26:23 -0500

    Added enemy self collision, Enemy spawn count limit, Debug logger method, Extracted some logic and magic to functions, Moved important variables and magic numbers to declared constants.

--- public/js/main.js
+++ public/js/main.js
@@ -27,6 +27,8 @@ var groundLayer, coinLayer;
 var text;
 var score = 0;
 
+const ENABLE_DEBUG_LOGGING = true;
+
 function preload() {
     // map made with Tiled in JSON format
     this.load.tilemapTiledJSON('map', 'assets/map.json');
@@ -59,12 +61,11 @@ function create() {
     this.physics.world.bounds.height = groundLayer.height;
 
     // create the player sprite    
-    player = this.physics.add.sprite(216, 216, 'player');
-    player.x = 200;
-    player.y = 375;
+    player = this.physics.add.sprite(PLAYER_SPRITE_SIZE.X, PLAYER_SPRITE_SIZE.Y, 'player');
+    player.x = PLAYER_SPAWN_LOCATION.X;
+    player.y = PLAYER_SPAWN_LOCATION.Y;
     player.body.setSize(player.width - 195, player.height);
-    player.setCollideWorldBounds(true); // don't go out of the map    
-
+    player.setCollideWorldBounds(true); // don't go out of the map
 
     // player will collide with the level tiles 
     this.physics.add.collider(groundLayer, player);
@@ -123,19 +124,61 @@ function collectCoin(sprite, tile) {
     return false;
 }
 
-let lastEnemySpawnTime;
+
+// TODO: Move these constants to the top? Wherever they work for David.
+const PLAYER_SPRITE_SIZE = {
+    X: 216,
+    Y: 216
+};
+
+const ENEMY_SPRITE_SIZE = {
+    X: 216,
+    Y: 216
+}
+
+const ENEMY_SPAWN_COUNT_LIMIT = 5;
+const ENEMY_SPAWN_TIME_MILLIS = 3_000;
+const PLAYER_MOVE_SPEED = 200;
+const ENEMIES_HAVE_ENEMY_COLLISION = true;
+
+const PLAYER_SPAWN_LOCATION = {
+    X: 200,
+    Y: 375
+};
+
+const ENEMY_TEST_SPAWN_LOCATION = {
+    X: 1200,
+    Y: 375
+};
+
+let lastEnemySpawnTime = 0;
+
 function spawnEnemy(context, time) {
+    let enemyCount = enemies.length;
+    if (enemyCount >= ENEMY_SPAWN_COUNT_LIMIT) {
+        debugLog(`Not spawning enemies due to cap of: ${enemyCount}`);
+        return;
+    }
+
+    debugLog(`Spawning enemy #${enemyCount + 1}`);
+
     lastEnemySpawnTime = time;
-    let enemy = context.physics.add.sprite(216, 216, 'player');
+
+    let enemy = context.physics.add.sprite(ENEMY_SPRITE_SIZE.X, ENEMY_SPRITE_SIZE.Y, 'player');
     enemy.body.setSize(enemy.width - 195, enemy.height);
     enemy.setCollideWorldBounds(true);
     context.physics.add.collider(groundLayer, enemy);
     context.physics.add.collider(player, enemy);
     enemy.flipX = true;
-    enemy.x = 1200;
-    enemy.y = 375;
-    enemy.body.setVelocityX(-50 * Math.random() - 50)
-    enemy.tint = '0x'+Math.floor(Math.random()*16777215).toString(16);
+    enemy.x = ENEMY_TEST_SPAWN_LOCATION.X;
+    enemy.y = ENEMY_TEST_SPAWN_LOCATION.Y;
+    enemy.body.setVelocityX(randomEnemyXVelocity());
+    enemy.tint = randomHexColor();
+
+    if (ENEMIES_HAVE_ENEMY_COLLISION) {
+        enemies.forEach(existingEnemy => context.physics.add.collider(existingEnemy, enemy));
+    }
+
     enemies.push(enemy);
 }
 
@@ -149,24 +192,23 @@ function update(time, delta) {
         currentFrame = nextFrame;
     }
 
-    if (time - lastEnemySpawnTime > 3_000) {
+    if (time - lastEnemySpawnTime > ENEMY_SPAWN_TIME_MILLIS) {
         spawnEnemy(this, time);
     }
 
-    enemies.forEach(enemy => 
-        enemy.anims.play('walk', true));
+    enemies.forEach(enemy => enemy.anims.play('walk', true));
 
     if (canPlayerMove()) {
         if (cursors.space.isDown) {
             isPlayerAttacking = true;
             timePlayerStartedAttack = time;
         } else if (cursors.left.isDown) {
-            player.body.setVelocityX(-200);
+            player.body.setVelocityX(-PLAYER_MOVE_SPEED);
             player.anims.play('walk', true); // walk left
             player.setOrigin(1, player.originY); // left-aligned sprites, so we set the origin to the far right when flipping.
             player.flipX = true; // flip the sprite to the left
         } else if (cursors.right.isDown) {
-            player.body.setVelocityX(200);
+            player.body.setVelocityX(PLAYER_MOVE_SPEED);
             player.anims.play('walk', true);
             player.setOrigin(0.5, player.originY); // Reset the origin to the middle when the left-aligned sprite faces right.
             player.flipX = false; // use the original sprite looking to the right
@@ -189,4 +231,18 @@ function update(time, delta) {
 
 function canPlayerMove() {
     return !isPlayerAttacking;
+}
+
+function randomHexColor() {
+    return '0x'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+function randomEnemyXVelocity() {
+    return -50 * Math.random() - 50;
+}
+
+function debugLog(message) {
+    if (ENABLE_DEBUG_LOGGING) {
+        console.log(message);
+    }
 }
