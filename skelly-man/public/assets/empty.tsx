ead29b0e6275befa4d08808d9bef4bff7b7e662c
Adam Shepley <adam.shepley@shopify.com>
2020-06-27 12:24:05 -0400

    Moved files into subfolders. DO NOT REBASE OR ERASE HISTORY BEFORE THIS COMMIT.

---
+++ skelly-man/public/assets/empty.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="empty" tilewidth="70" tileheight="70" tilecount="1" columns="1">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="empty.png" trans="ff00ff" width="128" height="128"/>
+</tileset>
