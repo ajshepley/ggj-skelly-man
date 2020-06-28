c89f9a64367ee5fde944afde1f9fe1a313fa31ea
Adam Shepley <adam.shepley@shopify.com>
2020-06-27 20:57:46 -0400

    Adjusted tab spacing for meter files.

--- hats/public/js/BossMeter.js
+++ hats/public/js/BossMeter.js
@@ -1,25 +1,25 @@
 "use strict";
 
 export class BossMeter {
-    constructor(phaser, x, y, width, height, color) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = color;
+  constructor(phaser, x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
 
-        this.outline = phaser.add.graphics();
-        const thickness = 4;
-        const alpha = 1;
-        this.outline.lineStyle(thickness, color, alpha);
-        this.outline.strokeRect(this.x, this.y, width, height);
+    this.outline = phaser.add.graphics();
+    const thickness = 4;
+    const alpha = 1;
+    this.outline.lineStyle(thickness, color, alpha);
+    this.outline.strokeRect(this.x, this.y, width, height);
 
-        this.fill = phaser.add.graphics();
-    }
+    this.fill = phaser.add.graphics();
+  }
 
-    updateFill(percentage) {
-        this.fill.clear();
-        this.fill.fillStyle(this.color, 1);
-        this.fill.fillRect(this.x, this.y, this.width * percentage, this.height);
-    }
+  updateFill(percentage) {
+    this.fill.clear();
+    this.fill.fillStyle(this.color, 1);
+    this.fill.fillRect(this.x, this.y, this.width * percentage, this.height);
+  }
 }
--- hats/public/js/SyncMeter.js
+++ hats/public/js/SyncMeter.js
@@ -1,24 +1,24 @@
 "use strict";
 
 export class SyncMeter {
-    constructor(phaser, x, y, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
+  constructor(phaser, x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
 
-        this.outline = phaser.add.graphics();
-        const thickness = 4;
-        const alpha = 1;
-        this.outline.lineStyle(thickness, color, alpha);
-        this.outline.strokeCircle(this.x, this.y, radius);
+    this.outline = phaser.add.graphics();
+    const thickness = 4;
+    const alpha = 1;
+    this.outline.lineStyle(thickness, color, alpha);
+    this.outline.strokeCircle(this.x, this.y, radius);
 
-        this.fill = phaser.add.graphics();
-    }
+    this.fill = phaser.add.graphics();
+  }
 
-    updateFill(percentage) {
-        this.fill.clear();
-        this.fill.fillStyle(this.color, 1);
-        this.fill.fillCircle(this.x, this.y, percentage * this.radius);
-    }
+  updateFill(percentage) {
+    this.fill.clear();
+    this.fill.fillStyle(this.color, 1);
+    this.fill.fillCircle(this.x, this.y, percentage * this.radius);
+  }
 }
